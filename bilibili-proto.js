8b11485b2ab52cecff669980fc1f7ee6a872fb8b
kdrbc <76228166+kdrbc@users.noreply.github.com>
2023-01-29 00:31:23 +0800

    Create bilibili-proto.js

--- bilibili-proto.js
+++ bilibili-proto.js
@@ -26,6 +26,61 @@ if (method !== "POST") {
     $notification.post(notifyTitle, "method错误:", method);
 }
 
+if(url.includes("Dynamic/DynAll")){
+    console.log('动态DynAll');
+    const dynAllReplyType = biliRoot.lookupType("bilibili.app.dynamic.DynAllReply");
+    let dynAllReplyObj = dynAllReplyType.decode(unGzipBody);
+    if(!dynAllReplyObj.topicList){
+        console.log('topicList为空');
+    } else {
+        needProcessFlag = true;
+        dynAllReplyObj.topicList = null;
+        console.log('推荐话题topicList去除');
+    }
+
+    if(!dynAllReplyObj.upList){
+        console.log('upList为空');
+    } else {
+        needProcessFlag = true;
+        dynAllReplyObj.upList = null;
+        console.log('最常访问upList去除');
+    }
+
+    if(!dynAllReplyObj.dynamicList?.list?.length){
+        console.log('动态列表list为空');
+    } else {
+        let adCount = 0;
+        dynAllReplyObj.dynamicList.list = dynAllReplyObj.dynamicList.list.filter(item => {
+            if(item.cardType !== 15){
+                return true;
+            }
+            adCount++;
+            return false;
+        });
+        if(adCount){
+            needProcessFlag = true;
+        }
+        console.log(`动态列表广告数量:${adCount}`);
+    }
+    if(needProcessFlag){
+        // let tagMapNullCount = 0;
+        // dynAllReplyObj.dynamicList.list.forEach(item => {
+        //     item.modules.forEach(mo => {
+        //         mo.moduleAuthor?.author.avatar.fallbackLayers.layers.forEach(lObj => {
+        //             const tagsMap = lObj.layerConfig.tags;
+        //             for (const i in tagsMap) {
+        //                 if(tagsMap[i] === null){
+        //                     // 解决tagsMap的null is not an object问题
+        //                     tagMapNullCount++;
+        //                     delete tagsMap[i];
+        //                 }
+        //             }
+        //         })
+        //     })
+        // })
+        // console.log(`tagsMap处理:${tagMapNullCount}`)
+        body = processNewBody(dynAllReplyType.encode(dynAllReplyObj).finish());
+    }
 } else if(url.includes("View/View")){
     console.log('视频播放页View/View');
     const viewReplyType = biliRoot.lookupType("bilibili.app.view.ViewReply");
